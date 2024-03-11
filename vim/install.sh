8d40e7a1866df18b9f10acd89b9ac569ae14ae49
hansanhha <kwixs35@gmail.com>
2024-03-11 13:03:03 +0900

    [vim] install 스크립트 수정

---
+++ vim/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+read -p "This way overwrite existing files in your home directory. Are you
+    sure? (y/n) " -n 1;
+
+echo "";
+
+if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    exit 0;
+fi;
+
+rsync -avh --no-perms . ~;
+
+# Check if vim is installed
+#
+if ! command -v vim > /dev/null 2>&1; then
+    echo "Vim is not installed. Installing..."
+    
+    # Check the Linux distro
+    if grep -qEi "(debian|ubuntu)" /etc/os-release; then
+        sudo apt-get update
+        sudo apt-get install vim -y
+    elif grep -qEi "(fedora|redhat)" /etc/os-release; then
+        sudo yum install vim -y
+    fi
+fi
+
+# Install vim-plug first
+echo "Installing plugin...\n"
+rm -rf /.vim/autoload/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
